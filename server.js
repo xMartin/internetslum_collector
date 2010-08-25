8237888c7fcee20a2569d4d0776455ebdff18058
martin <martin@siarp.de>
2010-08-25 17:56:19 +0200

    configurable port, variable ip and no semicolons

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-var http = require('http');
+var http = require('http')
+
+var port = process.argv[2] || 8080
+var ip = '127.0.0.1'
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8124, "127.0.0.1");
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('Internetslum\n')
+}).listen(port, ip)
 
-console.log('Server running at http://127.0.0.1:8124/');
+console.log('Server running at http://' + ip + ':' + port + '/')
