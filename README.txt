d34caf552299bf1072cee157baaf13957952d71a
martin <martin@siarp.de>
2010-08-25 20:16:25 +0200

    read from mongo db

---
+++ README.txt
@@ -0,0 +1,20 @@
+# install ports
+sudo port install nodejs mongodb
+
+# install node.js package manager "npm"
+curl http://npmjs.org/install.sh | sudo sh
+
+# install node.js module for MongoDB
+sudo npm install mongodb
+
+# setup and start database
+mkdir /Users/martin/node.js/mongodb/internetslum_collector
+/opt/local/bin/mongod --dbpath=/Users/martin/node.js/mongodb/internetslum_collector
+
+# fill database
+mongo
+> db.urls.save({url: 'http://www.mongodb.org/display/DOCS/Quickstart+OS+X'})
+> db.urls.save({url: 'http://www.slideshare.net/ggoodale/getting-started-with-mongodb-and-nodejs'})
+
+# start node.js server
+node server.js
--- server.js
+++ server.js
@@ -1,38 +1,53 @@
 var http = require('http')
 var url = require('url')
+var mongo = require('mongodb')
 
 var port = process.argv[2] || 8080
 var ip = '127.0.0.1'
 
-http.createServer(function(req, res) {
-  var url_parts = url.parse(req.url)
+console.log('Starting server...')
 
-  switch(url_parts.pathname) {
-    case '/':
-  	  display_root(url_parts.pathname, req, res)
-  	  break
-    case '/edit':
-  	  display_edit(url_parts.pathname, req, res)
-  	  break
-    default:
-  	  display_404(url_parts.pathname, req, res)
-  }
+var db = new mongo.Db('test', new mongo.Server('127.0.0.1', 27017, {}), {})
+db.open(function() {
+  db.collection('urls', function(err, collection) {
+    http.createServer(function(req, res) {
+      var url_parts = url.parse(req.url)
 
-  function display_root(url, req, res) {
-  	res.writeHead(200, {'Content-Type': 'text/html'})
-  	res.end('root')
-	}
+      switch(url_parts.pathname) {
+        case '/':
+      	  display_root(url_parts.pathname, req, res)
+      	  break
+        case '/list':
+      	  display_list(url_parts.pathname, req, res)
+      	  break
+        default:
+      	  display_404(url_parts.pathname, req, res)
+      }
 
-  function display_edit(url, req, res) {
-  	res.writeHead(200, {'Content-Type': 'text/html'})
-  	res.end('edit')
-	}
+      function display_root(url, req, res) {
+      	res.writeHead(200, {'Content-Type': 'text/html'})
+      	res.end('root')
+    	}
 
-  function display_404(url, req, res) {
-  	res.writeHead(404, {'Content-Type': 'text/html'})
-  	res.end('not found')
-  }
+      function display_list(url, req, res) {
+        var result = 'result:'
+      	res.writeHead(200, {'Content-Type': 'text/html'})
+      	collection.find({}, function(err, cursor) {
+          cursor.toArray(function(err, items) {
+            items.forEach(function(item) {
+              result = result + ' ' + item.url
+            })
+            res.end(result)
+          })
+        })
+    	}
 
-}).listen(port, ip)
+      function display_404(url, req, res) {
+      	res.writeHead(404, {'Content-Type': 'text/html'})
+      	res.end('not found')
+      }
+    }).listen(port, ip)
+  })
+})
 
 console.log('Server running at http://' + ip + ':' + port + '/')
